a627a67b09bff6a10492f82ca0652ef0b08a4b14
Gregory Huff <gregory.huff@capitalone.com>
2014-12-05 12:58:07 -0500

    Remove references that aren't used

--- update.sh
+++ update.sh
@@ -19,10 +19,6 @@ read -p "Update packages manually and press any key... " -n 1
 sudo chown -R "$(whoami)" ~/.npm
 sudo chown -R "$(whoami)" /usr/local
 
-## cabal
-cabal update
-cabal install cabal-install --force-reinstalls
-
 ## rvm
 rvm get stable
 
@@ -31,18 +27,7 @@ sudo gem update --system
 sudo gem update
 sudo gem cleanup
 
-## zsh
-upgrade_oh_my_zsh
-
 # finish
 diskutil repairPermissions /
 brew doctor
 read -p "Fixed problem and press any key... " -n 1
-
-
-## clean system
-read -p "System Updated. Clean now? (y/n) " -n 1
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  . clean
-fi
