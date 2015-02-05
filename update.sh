07a58779e17a3ce45e7feef1f0c80c9e7f72ef2f
Gregory Huff <gregory.huff@capitalone.com>
2015-02-05 10:18:40 +0000

    Just adjusting the order at the end

--- update.sh
+++ update.sh
@@ -47,5 +47,5 @@ sudo pear upgrade-all
 
 ## finish
 diskutil repairPermissions /
-brew doctor
 rm ~/Library/Application\ Support/Dock/*.db; defaults write com.apple.dock ResetLaunchPad -bool true; killall Dock
+brew doctor
