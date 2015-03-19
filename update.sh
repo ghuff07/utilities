5c7c56cfd5f5de87dd818abacc3339503c25a767
Gregory Huff <gregory.huff@capitalone.com>
2015-03-19 18:54:05 +0000

    Remove dot files from rum

--- update.sh
+++ update.sh
@@ -32,7 +32,7 @@ sudo chown -R "$(whoami)" /usr/local
 
 ## rvm
 rvm fix-permissions
-rvm get stable --auto-dotfiles
+rvm get head
 rvm requirements
 rvm rubygems latest
 rvm all do sudo gem update --system
