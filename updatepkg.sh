6e6d1b3dca5f2b328bb206deb316928a8535a526
Gregory Huff <gregory.huff@capitalone.com>
2015-12-18 09:55:29 -0500

    Added license-checker

--- updatepkg.sh
+++ updatepkg.sh
@@ -63,6 +63,7 @@ npm install -g grunt-cli
 npm install -g gulp
 npm install -g jshint
 npm install -g less
+npm install -g license-checker
 npm install -g nodemon
 npm install -g nsp
 npm install -g phonegap
