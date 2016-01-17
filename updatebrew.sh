8781389f0f3db73c627d077ee299b09388f20ac7
Gregory Huff <gregory.huff@gmail.com>
2016-01-17 17:01:33 -0500

    Added new scripts for reinstalling brew packages and brew cask packages

---
+++ updatebrew.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo -v
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+sudo chflags norestricted /usr/local && sudo chown -R $(whoami):admin /usr/local && sudo chown -R $(whoami):staff /Users/$(whoami)
+
+for c in `brew list`; do ! brew info $c || brew reinstall $c; done
