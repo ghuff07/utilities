2d8bdebba6068b88d463d550b1c0c6bcbd08e8b3
Gregory Huff <gregory.huff@gmail.com>
2016-07-20 14:05:02 -0400

    removed unnecessary steps

--- update.sh
+++ update.sh
@@ -20,8 +20,6 @@ git fetch origin
 git reset --hard origin/master
 brew update
 brew cask update
-brew unlink php56
-brew unlink php70
 brew upgrade --all
 for c in `brew cask list`; do ! brew cask info $c | grep -qF "Not installed" || brew cask install $c; done
 brew linkapps
@@ -44,7 +42,6 @@ bower update
 
 # rvm
 rvm get head
-rvm requirements
 rvm rubygems latest
 rvm all do gem update --system
 rvm all do gem update
@@ -58,50 +55,26 @@ pip install --upgrade pip setuptools
 pip freeze --local | grep -v '^\-e' | cut -d = -f 1 | xargs pip install -U
 
 # pip3
-pip3 install --upgrade pip setuptools
+pip3 install --upgrade pip setuptools wheel
 pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1 | xargs pip3 install -U
 
 # pear
-source $(brew --prefix php-version)/php-version.sh && php-version 5
-brew info php56 | grep /usr/local/Cellar/php56 | head -n1 | cut -d \  -f 1 | cut -c25-27 | xargs -I version sh -c "rm /usr/local/etc/php/version/pear.conf"
-brew info php56 | grep /usr/local/Cellar/php56 | head -n1 | cut -d \  -f 1 | xargs -I path sh -c "chmod -R ug+w path/lib/php"
-brew info php56 | grep /usr/local/Cellar/php56 | head -n1 | cut -d \  -f 1 | cut -c25-27 | xargs -I version sh -c "pear config-set php_ini /usr/local/etc/php/version/php.ini system"
+#brew info php70 | grep /usr/local/Cellar/php70 | head -n1 | cut -d \  -f 1 | cut -c25-27 | xargs -I version sh -c "rm /usr/local/etc/php/version/pear.conf"
+brew info php70 | grep /usr/local/Cellar/php70 | head -n1 | cut -d \  -f 1 | xargs -I path sh -c "chmod -R ug+w path/lib/php"
+brew info php70 | grep /usr/local/Cellar/php70 | head -n1 | cut -d \  -f 1 | cut -c25-27 | xargs -I version sh -c "pear config-set php_ini /usr/local/etc/php/version/php.ini system"
 pear config-set auto_discover 1
 pear upgrade pear
 pear clear-cache
 pear update-channels
 pear upgrade
-
-source $(brew --prefix php-version)/php-version.sh && php-version 7
-brew info php71 | grep /usr/local/Cellar/php70 | head -n1 | cut -d \  -f 1 | cut -c25-27 | xargs -I version sh -c "rm /usr/local/etc/php/version/pear.conf"
-brew info php71 | grep /usr/local/Cellar/php70 | head -n1 | cut -d \  -f 1 | xargs -I path sh -c "chmod -R ug+w path/lib/php"
-brew info php71 | grep /usr/local/Cellar/php70 | head -n1 | cut -d \  -f 1 | cut -c25-27 | xargs -I version sh -c "pear config-set php_ini /usr/local/etc/php/version/php.ini system"
-pear config-set auto_discover 1
-pear upgrade pear
-pear clear-cache
-pear update-channels
-pear upgrade
-
-# composer
-composer self-update
 
 # go
 go get -u all
 
-# android
-android update adb
-
 # java
-if which jenv > /dev/null; then eval "$(jenv init -)"; fi
-jenv rehash
-jenv enable-plugin export
-
-# python
-pyenv rehash
-
-# quicklook
-qlmanage -r cache
-qlmanage -r
+#if which jenv > /dev/null; then eval "$(jenv init -)"; fi
+#jenv rehash
+#jenv enable-plugin export
 
 # security tools
 bundle-audit update
@@ -110,8 +83,7 @@ bundle-audit update
 rm ~/Library/Application\ Support/Dock/*.db; rm -rf "$TMPDIR../0/com.apple.dock.launchpad/db"; defaults write com.apple.dock ResetLaunchPad -bool true; sudo killall -SIGKILL cfprefsd && killall Dock && killall Finder
 
 # various checkups
-jenv doctor
+#jenv doctor
 yo doctor
-appium-doctor
 brew cask doctor
 brew doctor
