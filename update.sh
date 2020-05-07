36df51256620534aa3de71042cd730ae8bdde432
Gregory Huff <gregory.huff@gmail.com>
2020-05-06 17:13:03 -0700

    updated the numbered steps

--- update.sh
+++ update.sh
@@ -16,37 +16,37 @@ shopt -s expand_aliases
 source ~/.aliases
 
 # App Store and Xcode maintenance
-echo -e "${LG}[1/18] Updating App Store apps${NC}"
+echo -e "${LG}[1/19] Updating App Store apps${NC}"
 mas outdated
 mas upgrade
 
-echo -e "${LG}[2/18] Verifying Xcode Command Line Tools${NC}"
+echo -e "${LG}[2/19] Verifying Xcode Command Line Tools${NC}"
 xcode-select --install
 
 # Homebrew maintenance
-echo -e "${LG}[3/18] Updating Homebrew${NC}"
+echo -e "${LG}[3/19] Prepping Homebrew${NC}"
 alias brew='PATH=${PATH//$(pyenv root)\/shims:/} brew'
 cd "$(brew --prefix)/homebrew"
 git fetch origin
 git reset --hard origin/master
 brew tap --repair
 
-echo -e "${LG}[4/18] Updating Homebrew formulae${NC}"
+echo -e "${LG}[4/19] Updating Homebrew formulae${NC}"
 brew update
 brew upgrade
 
-echo -e "${LG}[5/18] Updating Homebrew casks${NC}"
+echo -e "${LG}[5/19] Updating Homebrew casks${NC}"
 brew cu -a -y --cleanup
 
-echo -e "${LG}[6/18] Recreating Homebrew Brewfile${NC}"
+echo -e "${LG}[6/19] Recreating Homebrew Brewfile${NC}"
 brew bundle dump --force
 
-echo -e "${LG}[7/18] Cleaning up Homebrew${NC}"
+echo -e "${LG}[7/19] Cleaning up Homebrew${NC}"
 brew cleanup -s
 brew services cleanup
 
 # dev environment maintenance
-echo -e "${LG}[8/18] Updating **envs and shims${NC}"
+echo -e "${LG}[8/19] Updating **envs and shims${NC}"
 anyenv install --update
 goenv rehash
 jenv rehash
@@ -57,57 +57,57 @@ sbtenv rehash
 scalaenv rehash
 
 # Python maintenance
-echo -e "${LG}[9/18] Updating Python packages${NC}"
+echo -e "${LG}[9/19] Updating Python packages${NC}"
 pip install --upgrade pip setuptools wheel
 pip freeze --local | grep -v '^\-e' | cut -d = -f 1 | xargs pip install -U
 
 # Node.js maintenance
-echo -e "${LG}[10/18] Updating Node.js packages${NC}"
+echo -e "${LG}[10/19] Updating Node.js packages${NC}"
 npm cache verify
 npm update -g
 
 # Ruby maintenance
-echo -e "${LG}[11/18] Updating Ruby gems${NC}"
+echo -e "${LG}[11/19] Updating Ruby gems${NC}"
 gem update --system
 gem update
 gem cleanup
 bundle-audit update
 
 # PHP maintenance
-echo -e "${LG}[12/18] Updating PHP packages${NC}"
+echo -e "${LG}[12/19] Updating PHP packages${NC}"
 pear clear-cache
 pecl clear-cache
 pear update-channels
 pecl update-channels
 
 # Go maintenance
-echo -e "${LG}[13/18] Updating Go packages${NC}"
+echo -e "${LG}[13/19] Updating Go packages${NC}"
 go get -u all
 go clean
 
 # Quick Look maintenance
-echo -e "${LG}[14/18] Reloading Quick Look generators${NC}"
+echo -e "${LG}[14/19] Reloading Quick Look generators${NC}"
 qlmanage -r
 qlmanage -r cache
 
 # MacOS system maintenance
-echo -e "${LG}[15/18] Running MacOS maintenance routines${NC}"
+echo -e "${LG}[15/19] Running MacOS maintenance routines${NC}"
 sudo periodic daily weekly monthly
 
 # MacOS system updates
-echo -e "\n${LG}[16/18] Updating MacOS${NC}"
+echo -e "\n${LG}[16/19] Updating MacOS${NC}"
 sudo softwareupdate -ia
 
 # Backup dot and config files
-echo -e "${LG}[17/18] Backing up dot and config files${NC}"
+echo -e "${LG}[17/19] Backing up dot and config files${NC}"
 mackup backup -f
 
 # Reset Launchpad
-echo -e "${LG}[18/18] Resetting Launchpad${NC}"
+echo -e "${LG}[18/19] Resetting Launchpad${NC}"
 defaults write com.apple.dock ResetLaunchPad -bool true
 killall Dock
 
 # Final checkups
-echo -e "${LG}[19/18] Running final health checks${NC}"
+echo -e "${LG}[19/19] Running final health checks${NC}"
 brew cask doctor
 brew doctor
